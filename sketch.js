5992e48abb62688d47bfb847172b6a876487b629
Smart10101 <69665651+Smart10101@users.noreply.github.com>
2020-10-29 19:11:06 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,16 +1,30 @@
-var wall, thickness; 
-var bullet, speed, weight, damage;
+var ball, wall;
+var speed, weight;
 
-thickness = random(22,83);
-
-weight = random(30,52);
-speed = random(223,321);
-
-function setup(){
-    wall = createSprite(1200,200,thickness, height/2);
+function setup() {
+  createCanvas(1600,400);
+  speed = random(55,90);
+  weight = random(400,1500);
+  car = createSprite(50,200,50,50);
+  wall = createSprite(1000,200,60,height/2);
+  wall.shapeColor = color(80,80,80);
+  car.velocityX = speed;
 }
 
-function draw(){
-    background(80,80,80);
-    damage = ((0.5)(weight)(speeed))/((thickness)(thicknes)(thickness))
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  if(wall.x-car.x < (car.width+wall.width)/2){
+    car.velocityX = 0;
+    var deformation = 0.5*weight*speed*speed/22500;
+    if(deformation > 180){
+      car.shapeColor = color(255,0,0);
+    }
+    if(deformation > 100 && deformation < 180){
+      car.shapeColor = color(230,230,0);
+    }
+    if(deformation < 100){
+      car.shapeColor = color(0,255,0);
+    }
+  }
 }
